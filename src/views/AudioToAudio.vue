c2f156b3be2a1bee4d4ccbe03a085c1c24add360
Rutvik Mehta <rutvik1061992@gmail.com>
2023-11-01 01:47:00 +0530

    navigator.audioSession.type = 'play-and-record';

--- src/views/AudioToAudio.vue
+++ src/views/AudioToAudio.vue
@@ -47,6 +47,8 @@ export default {
                 audio: true,
             });
 
+            navigator.audioSession.type = 'play-and-record';
+
             return new MediaRecorder(userMedia);
 
         },
